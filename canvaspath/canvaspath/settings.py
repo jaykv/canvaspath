1c6903677ceb926037af9ce09f9a915ad7be1326
Jay <improcuziam@gmail.com>
2019-04-04 03:34:17 -0400

    setup app and basic login

--- canvaspath/canvaspath/settings.py
+++ canvaspath/canvaspath/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
+    'coursesystem',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
@@ -81,6 +82,28 @@ DATABASES = {
 }
 
 
+# some logging
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+       'file': {
+           'level': 'DEBUG',
+           'class': 'logging.FileHandler',
+           'filename': 'log.django',
+       },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console','file'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
@@ -105,7 +128,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'EST'
 
 USE_I18N = True
 
@@ -114,7 +137,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+LOGIN_REDIRECT_URL = '/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
--- canvaspath/coursesystem/views.py
+++ canvaspath/coursesystem/views.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+	return render(request,'coursesystem/home.html')
+
+def signup(request):
+	form = UserCreationForm
+	successurl = reverse_lazy('login')
+	return render(request, 'coursesystem/signup.html', {'form': form})
