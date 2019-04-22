8ab981f99e74ea88365707549d23412349c974d1
Jay <improcuziam@gmail.com>
2019-04-22 14:14:59 -0400

    update schema

--- canvaspath/coursesystem/models.py
+++ canvaspath/coursesystem/models.py
@@ -16,9 +16,17 @@ class Student(models.Model):
 	street = models.TextField()
 	zipcode = models.ForeignKey(Zipcode, on_delete=None)
 
+	class Meta:
+		unique_together = (('user', 'email'),)
+
+
 class Department(models.Model):
 	dept_name = models.TextField()
 	dept_head = models.TextField()
+
+	class Meta:
+		unique_together = (('dept_name', 'dept_head'),)
+
 
 class Professor(models.Model):
 	user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -30,36 +38,53 @@ class Professor(models.Model):
 	department = models.ForeignKey(Department, on_delete=None)
 	title = models.TextField()
 
+	class Meta:
+		unique_together = (('user', 'email'),)
+
+
 class Course(models.Model):
 	course_id = models.TextField(primary_key=True)
 	course_name = models.TextField()
 	course_description = models.TextField()
 
-
 class Prof_teams(models.Model):
 	teaching_team_id = models.IntegerField(primary_key=True)
 
+class Prof_team_members(models.Model):
+	prof_email = models.TextField()
+	teaching_team_id = models.ForeignKey(Prof_teams, on_delete=models.CASCADE)
+
+	class Meta:
+		unique_together = (('prof_email', 'teaching_team_id'),)
+
 class Sections(models.Model):
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-	sec_no = models.IntegerField(primary_key = True)
+	sec_no = models.IntegerField()
 	section_type = models.TextField()
 	limit = models.IntegerField()
 	prof_team = models.ForeignKey(Prof_teams, on_delete=None)
 
+	class Meta:
+		unique_together = (('course_id', 'sec_no'),)
+
+
 class Enrolls(models.Model):
 	student_email = models.TextField()
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-	section_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
+	sec_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
 
-class Prof_team_members(models.Model):
-	prof_email = models.TextField()
-	team_id = models.ForeignKey(Prof_teams, on_delete=models.CASCADE)
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'student_email'),)
 
 class Homework(models.Model):
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
 	sec_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
 	hw_no = models.IntegerField()
 	hw_details = models.TextField()
+
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'hw_no'),)
+
 
 class Homework_grades(models.Model):
 	student_email = models.TextField()
@@ -68,11 +93,19 @@ class Homework_grades(models.Model):
 	hw_no = models.ForeignKey(Homework, on_delete=models.CASCADE)
 	hw_grade = models.IntegerField()
 
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'student_email', 'hw_no'),)
+
+
 class Exams(models.Model):
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
 	sec_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
 	exam_no = models.IntegerField()
 	exam_details = models.TextField()
+
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'exam_no'),)
+
 
 class Exam_grades(models.Model):
 	student_email = models.TextField()
@@ -81,26 +114,45 @@ class Exam_grades(models.Model):
 	exam_no = models.IntegerField()
 	exam_grade = models.IntegerField()
 
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'student_email', 'exam_no'),)
+
+
 class Capstone_section(models.Model):
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
 	sec_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
 	project_no = models.IntegerField()
 	sponsor_id = models.EmailField()
 
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'project_no'),)
+
+
 class Capstone_Team(models.Model):
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
 	sec_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
-	capstone_team_id = models.TextField(primary_key=True)
+	capstone_team_id = models.IntegerField()
 	project_no = models.IntegerField()
+
+	class Meta:	
+		unique_together = (('course_id', 'sec_no', 'capstone_team_id'),)
+
 
 class Capstone_Team_Members(models.Model):
 	student_email = models.EmailField(max_length=70)
 	capstone_team_id = models.ForeignKey(Capstone_Team, on_delete=models.CASCADE)
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-	sec_no =models.ForeignKey(Sections, on_delete=models.CASCADE)
+	sec_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
+
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'student_email', 'capstone_team_id'),)
+
 
 class Capstone_grades(models.Model):
 	course_id = models.ForeignKey(Course, on_delete=models.CASCADE)
-	sec_no =models.ForeignKey(Sections, on_delete=models.CASCADE)
+	sec_no = models.ForeignKey(Sections, on_delete=models.CASCADE)
 	capstone_team_id = models.ForeignKey(Capstone_Team, on_delete=models.CASCADE)
 	capstone_grade = models.IntegerField()
+
+	class Meta:
+		unique_together = (('course_id', 'sec_no', 'capstone_team_id'),)
