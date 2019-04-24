a0a21a6dbd6c834c6f27037ec01d1db9577ee31e
Jay <improcuziam@gmail.com>
2019-04-24 05:51:34 -0400

    update login template

--- canvaspath/canvaspath/urls.py
+++ canvaspath/canvaspath/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf.urls import url
+from django.contrib.auth.views import LoginView
 from django.urls import include, path
 
 urlpatterns = [
     path('siteadmin/', admin.site.urls),
+    url(r'^accounts/login/$', LoginView, name='login'),
     path('', include('django.contrib.auth.urls')),
     path('', include('coursesystem.urls')),
 ]
